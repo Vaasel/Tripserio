f0d8f1e4bbb38cf552545cc66eef314cd06b40cf
Muhammad Haris Azaz Khan <77955039+haris-khan1596@users.noreply.github.com>
2023-07-25 14:40:39 +0500

    Update blogRoutes.js

--- backend/routes/blogRoutes.js
+++ backend/routes/blogRoutes.js
@@ -7,10 +7,10 @@ const validationMiddleware = require("../middlewares/validationMiddleware");
 
 
 router.post("/addBlog", multerUpload, cloudinaryMiddleware, blogController().addBlog);
-router.get("/getBlogs", blogController().getBlogs);
 router.get("/getBlog/:id", blogController().getBlog);
 router.put("/editBlog/:id", multerUpload, blogController().editBlog);
-router.delete("/deleteBlog/:id", blogController().deleteBlog)
+router.delete("/deleteBlog/:id", blogController().deleteBlog);
+router.get("/", blogController().getBlogs);
 
 
 
