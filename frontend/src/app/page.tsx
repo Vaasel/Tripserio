7740f57b687f3c3865eab89540e48129d7b6d3eb
captainWaheed <thecaptain0078@gmail.com>
2023-07-30 20:40:12 +0500

    ✨ feat: Hero Section added

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,14 @@
+import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
-
+import RootLayout from "@/app/layout";
 export default function Home() {
   return (
     <>
-      <header className=" w-full h-full  object-cover">
-        <Navbar />
+      <header className=" flex justify-center items-center h-screen ">
+        <Hero />
       </header>
     </>
   );
 }
+
+Home.RootLayout = RootLayout;
