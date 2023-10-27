31c9b574fbc4f8bdf07613398a0ceaec026164f2
VA-vaasel <135807290+VA-vaasel@users.noreply.github.com>
2023-10-27 19:47:44 +0500

    Update conn.js

--- backend/db/conn.js
+++ backend/db/conn.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
+const password = encodeURIComponent("VeWfo5KQ9ZSPtpBy");
+const uri = "mongodb+srv://Harry:" + password + "@vaasel.0ximno3.mongodb.net/Tripserio?retryWrites=true&w=majority";
 
-async function dbConnect(uri) {
+async function dbConnect() {
     try {
         await mongoose.connect(uri, {
             useNewUrlParser: true,
